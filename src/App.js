9d2b2e29ac06f1c5a164248e61c194ff3ebd700b
Renato PL <renapl90@gmail.com>
2018-05-24 17:35:32 +0200

    basic structure before switching to itunes api

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props){
+        super(props);
+        this.state = {
+            query: ''
+        }
+    }
+
+    search(){
+        console.log(this.state);
+        const BASE_URL = 'https://api.spotify.com/v1/search?'
+        const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+
+        console.log(FETCH_URL);
+    }
+
+    keyPress(event){
+        if (event.key === 'Enter'){
+            this.search()
+        }
+    }
+
+    render(){
+        return(
+            <div className="App">
+                <div className="App-title">itunes</div>
+                <FormGroup>
+                    <InputGroup>
+                        <FormControl
+                            type="text"
+                            placeholder="Search an artist..."
+                            value={this.state.query}
+                            onChange={event => {this.setState({query: event.target.value})}}
+                            onKeyPress={(event) => this.keyPress(event)}
+                        />
+                        <InputGroup.Addon onClick={() => this.search()}>
+                            <Glyphicon glyph="search"></Glyphicon>
+                        </InputGroup.Addon>
+                    </InputGroup>
+                </FormGroup>
+                <div className="Profile">
+                    <div>Artist Picture</div>
+                    <div>Artist Name</div>
+                </div>
+                <div className="Gallery">
+                    Gallery
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;

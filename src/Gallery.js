325738470b52626645f1402f1b7d401be0d403f1
Renato PL <renapl90@gmail.com>
2018-05-25 16:30:08 +0200

    playable audio preview

--- src/Gallery.js
+++ src/Gallery.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import './App.css'
 
 class Gallery extends Component {
+    playAudio(previewUrl) {
+        let audio = new Audio(previewUrl);
+        audio.play();
+    }
+
     render(){
         console.log(this.props);
         const { tracks } = this.props;
@@ -11,7 +16,7 @@ class Gallery extends Component {
                     tracks.map((track, k) => {
                         const trackImg = track.album.images[0].url;
                         return(
-                            <div key={k} className="track">
+                            <div key={k} className="track" onClick={() => this.playAudio(track.preview_url)}>
                                 <img src={trackImg} alt="track" className="track-img"/>
                                 <p className="track-text">{track.name}</p>
                             </div>

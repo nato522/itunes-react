5cf84c40416d71489278717487761115d318d67b
Renato PL <renapl90@gmail.com>
2018-05-25 14:57:59 +0200

    readme updated

---
+++ src/credentials.js
@@ -0,0 +1,6 @@
+const tokens = {
+    ACCESS_TOKEN: 'BQBxGdD-voL9_9oMCvZYH0-4qLfedSdwX0G9tsRoRGVemtIOlSBOQYJE64XLcvZsSBhvMrwziNlqKbDLcDjdEvaVjBWO_HbLuP4KNgdKRDnHOCq7p-H4ZPl5nKEIpGJsljSF5yA7H1BYoIabf5ER915XgsDNUg',
+    REFRESH_TOKEN: 'AQBK2DCeoRwRNuN-8BaaIC-VSRmgr9WBO6HlebORoUL5-6-MFif8W7IedERKKdinATkMazHH8I-cxRujUSurbihZlTBzLWwaUyLcV2dafu8_iW51jPGQ4ilgkFouilwKcnI',
+}
+
+export default tokens;
